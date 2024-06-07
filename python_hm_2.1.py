16bd2f0904ae49ca5ccd7a655b844fcb352c114e
Denisbers <denis16031984@gmail.com>
2024-06-07 23:21:20 +0300

    commit

---
+++ python_hm_2.1.py
@@ -0,0 +1,11 @@
+digit_input = int(input('Please enter a four-digit number:'))
+
+digit_1 = digit_input // 1000
+digit_2 = (digit_input // 100) % 10
+digit_3 = (digit_input % 100) // 10
+digit_4 = digit_input % 10
+
+print(digit_1)
+print(digit_2)
+print(digit_3)
+print(digit_4)
